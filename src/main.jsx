50f77c3119fcd8bada2ed6e1442b430956a909a6
Mind1a <mindiaarabuli4@gmail.com>
2022-12-16 22:47:03 +0400

    core/fileStructure

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { GlobalStyle } from "./config/Global.styled";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <GlobalStyle />
     <App />
   </StrictMode>
 );
