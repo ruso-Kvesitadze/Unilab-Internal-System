a681751ab281d8490a7aad1e5abdc2ebc5c70b16
etherbits <nika.qvrivishviliPC@gmail.com>
2022-12-19 23:48:13 +0400

    NEW: adding authentication view

---
+++ src/views/Authentication/Authentication.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Authentication = () => {
+  return (
+    <div>
+      <form>
+        <h3>გამარჯობა 👋</h3>
+        <p>უნილაბის სამართავ პანელში მოსახვედრად, გთხოვთ გაიაროთ ავტორიზაცია</p>
+      </form>
+    </div>
+  )
+}
