4dfa56f2ca75ccce3d135e02f37d153e455a3224
etherbits <nika.qvrivishviliPC@gmail.com>
2022-12-23 11:29:04 +0400

    NEW: adding phone number input

---
+++ src/components/PhoneNumberInput/PhoneNumberInput.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const SCountryCode = styled.span`
+  display: flex;
+  height: 30px;
+  padding-right: 0.625rem;
+  font-size: 0.875rem;
+  align-items: center;
+  color: #737373;
+  border-right: solid 0.5px #dadada;
+`;
--- src/views/Registration/Registration.jsx
+++ src/views/Registration/Registration.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { SecondHeader } from "../../components/SecondHeader";
+import { PhoneNumberInput } from "../../components/PhoneNumberInput";
 export const Registration = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+
   return (
     <div>
       <SecondHeader />
+      <PhoneNumberInput
+        label="მობილურის ნომერი"
+        width="18.75rem"
+        fontSize="1rem"
+        countryCode={995}
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
+      />
     </div>
   );
 };
