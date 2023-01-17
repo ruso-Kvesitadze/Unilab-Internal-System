8e450d6da1e154c30e019c3e508a738a01295452
Mind1a <mindiaarabuli4@gmail.com>
2023-01-18 01:11:51 +0400

    core/MainPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { MainPage } from "./views/MainPage";
 import { Authentication } from "./views/Authentication";
 import { PrivacyPolicy } from "./views/PrivacyPolicy";
 import { RecoveryPassword } from "./views/RecoveryPassword";
@@ -14,7 +15,8 @@ const App = () => {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Authentication />} />
+          <Route path="/" element={<MainPage />} />
+          <Route path="/authentication" element={<Authentication />} />
           <Route path="/recovery-password" element={<RecoveryPassword />} />
           <Route path="/registration" element={<Registration />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
