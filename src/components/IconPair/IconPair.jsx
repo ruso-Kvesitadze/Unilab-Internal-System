0f28bd982545261f248789c736f2617c5b601fbe
etherbits <nika.qvrivishviliPC@gmail.com>
2023-01-19 03:34:53 +0400

    UPDATE: converting older pixel based file formats to newer webp format

--- src/components/IconPair/IconPair.jsx
+++ src/components/IconPair/IconPair.jsx
@@ -4,7 +4,7 @@ import { SIconPair } from "./IconPair.styled";
 export const IconPair = ({ width, height, gap, margin }) => {
   return (
     <SIconPair width={width} height={height} gap={gap} margin={margin}>
-      <img src="assets/png/unilab.png" alt="unilab" />
+      <img src="assets/png/unilab.webp" alt="unilab" />
       <img src="assets/svg/iliaUniLogo.svg" alt="iliaUniLogo" />
     </SIconPair>
   );
